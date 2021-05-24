4c913bc8f09c984f18c2843173131d9e68e8f4d4
2236739430 <2236739430@qq.com>
2021-05-24 17:37:29 +0800

    提交

---
+++ prob_test/prob_test/test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ostream>
+using namespace std;
+
+//void add(int a, int b,int c = 10)
+//{
+//	printf("%d\n",a + b);
+//}
+
+void add(int a, int b,int c)
+{
+	printf("%d\n", a + b);
+}
+
+int main()
+{
+	//add(10,10,10);
+	int a = 0;
+	const double& b = a;
+
+
+
+	return 0;
+}
